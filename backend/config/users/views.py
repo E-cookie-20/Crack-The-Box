3bca68115eef7decbd209ad815711783026f0b96
rkdmsslasla <gaeun9477@gmail.com>
2024-03-29 21:55:07 +0900

    회원가입 임시저장

--- backend/config/users/views.py
+++ backend/config/users/views.py
@@ -2,7 +2,20 @@ from .models import UserProfile
 from .serializers import UsersSerializer
 from rest_framework import viewsets
 
+from django.urls import reverse_lazy
+from django.views import generic
+from ..users.forms import CustomUserCreationForm
 
 class MembersViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UsersSerializer
+
+
+from django.shortcuts import render
+
+
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')  # 회원가입 성공 후 리디렉션할 URL
+    template_name = 'signup.html'  # 사용할 템플릿 파일명
