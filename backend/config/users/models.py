a284b81890d20a988700c09719a3e366321c92fa
rkdmsslasla <gaeun9477@gmail.com>
2024-03-29 20:06:32 +0900

    Guild 모델 생성

--- backend/config/users/models.py
+++ backend/config/users/models.py
@@ -28,10 +28,15 @@ class UserProfile(models.Model):
 
     
 # 길드 정보
-# class Guild(models.Model):
-#     guild_id = models.CharField(max_length=100, unique=True)
-#     name = models.CharField(max_length=100)
-#     # 추가 필드...
+class Guild(models.Model):
+    guild_id = models.CharField(max_length=100, unique=True)
+    guild_quiz_list = models.JSONField(default=list)
+    ctf_id = models.CharField(max_length=100)
+    guild_name = models.CharField(max_length=100)
+    guild_birth = user_birth = models.DateField()
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.guild_name
+
+
+
