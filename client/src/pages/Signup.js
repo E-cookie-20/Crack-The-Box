4f73c0855aec0039719aa658ce094ab55ba1d16e
박지은 <31093793+lucykorea414@users.noreply.github.com>
2024-03-29 19:51:05 +0900

    로그인, 회원가입 html

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,5 +1,20 @@
+import React from 'react';
+
 const Signup = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form action="signup.php" method="post">
+        <label htmlFor="signup_username">Username:</label><br />
+        <input type="text" id="signup_username" name="signup_username" required /><br />
+        <label htmlFor="signup_password">Password:</label><br />
+        <input type="password" id="signup_password" name="signup_password" required /><br />
+        <label htmlFor="confirm_password">Confirm Password:</label><br />
+        <input type="password" id="confirm_password" name="confirm_password" required /><br /><br />
+        <input type="submit" value="Signup" />
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
