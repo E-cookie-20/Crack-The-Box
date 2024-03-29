7d489a50fea9105a15c7dfebfc95a4bde3440021
rkdmsslasla <gaeun9477@gmail.com>
2024-03-29 20:10:58 +0900

    guild 모델 코드 이동

--- backend/config/users/models.py
+++ backend/config/users/models.py
@@ -26,17 +26,7 @@ class UserProfile(models.Model):
         return self.user.username
     
 
-    
-# 길드 정보
-class Guild(models.Model):
-    guild_id = models.CharField(max_length=100, unique=True)
-    guild_quiz_list = models.JSONField(default=list)
-    ctf_id = models.CharField(max_length=100)
-    guild_name = models.CharField(max_length=100)
-    guild_birth = user_birth = models.DateField()
 
-    def __str__(self):
-        return self.guild_name
 
 
 
