17d2eaa49f7d4959791e1da27741477c7fd74a91
rkdmsslasla <gaeun9477@gmail.com>
2024-03-29 18:50:44 +0900

    UserProfile 모델 생성
    장고 기본 User 모델을 OneToOneField로 연결하여 확장

--- backend/config/users/models.py
+++ backend/config/users/models.py
@@ -1,17 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Users(models.Model):
-    year = models.CharField(max_length=3, null=True, blank=False)#필수 데이터 
-    img = models.ImageField(upload_to='static/members/',blank=True) #이미지명은 학번 기준으로 하면 관리하기 좋을듯
-    std_id=models.IntegerField(unique=True)
-    name = models.CharField(max_length=100,unique=False)
-    email = models.EmailField(null=True, blank=True, unique=True)
-    comment = models.TextField(max_length=120, blank=True,unique=False)#공백 가능
-    insta_link = models.URLField(blank=True,unique=False)
-    git_link = models.URLField(null=True, blank=True,unique=False)
-    linkedin_link = models.URLField(blank=True,unique=False)
-    tf=models.BooleanField(default=False, unique=False)
-
+class UserProfile(models.Model):
+    # User 모델을 OneToOneField로 연결하여 확장
+    # 기본 User에 사용자 id(id), 이메일(email), 이름(username)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    # 길드 ID (예: CharField 사용, 필요에 따라 unique=True 추가 가능)
+    guild_id = models.CharField(max_length=100, blank=True, null=True)
+    
+    # 생년월일
+    user_birth = models.DateField()
+    
+    # 전화번호
+    user_phone = models.CharField(max_length=20, blank=True, null=True)
+    
+    # 성별 (예: 'M' for male, 'F' for female)
+    user_gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')))
+    
+    # 푼 문제 (JSONField 사용 or ManyToManyField로 별도의 문제 모델과 연결 가능)
+    user_quiz_solve = models.JSONField(default=list)
+    
 
     def __str__(self):
-        return self.name
+        return self.user.username
+    
+
+    
+# 길드 정보
+# class Guild(models.Model):
+#     guild_id = models.CharField(max_length=100, unique=True)
+#     name = models.CharField(max_length=100)
+#     # 추가 필드...
+
+#     def __str__(self):
+#         return self.name
--- backend/config/users/serializers.py
+++ backend/config/users/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from .models import Users
+from .models import UserProfile
 
 class UsersSerializer(serializers.ModelSerializer):
     img = serializers.ImageField(use_url=True)
     
     class Meta:
-        model = Users
+        model = UserProfile
         fields = '__all__'
