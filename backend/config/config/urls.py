571438d8514cc39bef45c0d04ebaa9d20dc4bb64
박지은 <31093793+lucykorea414@users.noreply.github.com>
2024-03-29 19:31:37 +0900

    wargame model, url, serializer 완료

--- backend/config/config/urls.py
+++ backend/config/config/urls.py
@@ -45,5 +45,6 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('wargame/', include('wargame.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
