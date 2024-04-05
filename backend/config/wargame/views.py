ad29828df3457c4dd1a148383a2088207464303b
박지은 <31093793+lucykorea414@users.noreply.github.com>
2024-04-05 20:15:07 +0900

    wargame create api 수정

--- backend/config/wargame/views.py
+++ backend/config/wargame/views.py
@@ -1,14 +1,56 @@
 from rest_framework import viewsets
-from .serializers import WargameSerializer
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+import json
+from django.http import HttpResponse
+
+from .serializers import WargameSerializer
 from .models import Wargame
+from .forms import WargameForm
 
 class WargameViewSet(viewsets.ModelViewSet):
     queryset = Wargame.objects.all()
     serializer_class = WargameSerializer
+
+# class WargameCreateSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Wargame
+#         fields = '__all__'
+
+#     def create(self, validated_data):
+#         # 현재 요청을 보낸 사용자를 작성자로 할당
+#         user = self.context['request'].user
+#         validated_data['author'] = user
+#         return super().create(validated_data)
+
+
+
+
+# class WargameCreateAPIView(APIView):
+#     def post(self, request, *args, **kwargs):
+#         serializer = WargameCreateSerializer(data=request.data, context={'request': request})
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+def wargame_upload(request):
+    if request.method == "POST":
+        form = WargameForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.instance.author = request.user
+            form.save()
+            return HttpResponse(json.dumps({"status": "Success"}))
+        else:
+            return HttpResponse(json.dumps({"status": "Failed"}))
+
+
+
+
 
 # 오류: "Method Not Allowed: /wargame/submit-flag/" -> 뭔가 POST 요청에 대한게 막혀있는것 같음...
 class SubmitFlagAPI(APIView):
