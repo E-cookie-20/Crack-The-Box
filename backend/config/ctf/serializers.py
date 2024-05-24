a6caa547154e560a01b41554bb4f757643ccffce
rosieposiess <haeun506@ewhain.net>
2024-05-24 19:58:34 +0900

    Edit urls

--- backend/config/ctf/serializers.py
+++ backend/config/ctf/serializers.py
@@ -6,6 +6,16 @@ class CTFSerializer(serializers.ModelSerializer):
         model = CTF
         fields = '__all__'
 
+    def to_representation(self, instance):
+        # request의 query parameter에서 'guild_id'를 가져옴
+        guild_id = self.context.get('request').query_params.get('guild_id')
+
+        if guild_id:
+            instance = CTF.objects.filter(guild_id=guild_id)
+
+        return super().to_representation(instance)
+
+
 class CTFchallengeSerializer(serializers.ModelSerializer):
     class Meta:
         model = CTF_challenge
--- backend/config/ctf/views.py
+++ backend/config/ctf/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
-from rest_framework import status,viewsets
+from rest_framework import status, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
-from .serializers import CTFSerializer, CTFchallengeSerializer,CTFUserSerializer
-from .models import CTF,CTF_challenge, CTF_user, User
+from .serializers import CTFSerializer, CTFchallengeSerializer, CTFUserSerializer
+from .models import CTF, CTF_challenge, CTF_user, User
 from .forms import challangeForm
-from django.db.models import F, Count
+from django.db.models import Count
 
 
+# CTFViewSet 정의: CTF 모델의 CRUD 작업을 위한 ViewSet
 class CTFViewSet(viewsets.ModelViewSet):
-    queryset = CTF_user.objects.all()
-    serializer_class = CTFSerializer
-    #serializer_class = CTFUserSerializer
+    queryset = CTF.objects.all()  # 모든 CTF 객체를 조회
+    serializer_class = CTFSerializer  # CTF 객체를 시리얼라이즈할 때 사용할 시리얼라이저 클래스 지정
+
+    # retrieve 메서드 오버라이드: 특정 CTF의 상세 정보 조회 시 호출
+    def retrieve(self, request, *args, **kwargs):
+        ctf_id = kwargs.get('pk')  # URL에서 ctf_id를 가져옴
+        ctf = get_object_or_404(CTF, pk=ctf_id)  # 해당 ctf_id의 CTF 객체를 조회, 없으면 404 반환
+        challenges = CTF_challenge.objects.filter(ctf_id=ctf)  # 해당 CTF에 속한 모든 챌린지 조회
+        challenge_serializer = CTFchallengeSerializer(challenges, many=True)  # 챌린지들을 시리얼라이즈
+        return Response(challenge_serializer.data)  # 시리얼라이즈된 챌린지 데이터를 응답으로 반환
 
     def top_users(self, request): #10명 내림차순으로 가져오기
         top_users = CTF_user.objects.annotate(rank=Count('user__user_pts') + 1).order_by('-user__user_pts')[:10].values('user__username', 'rank', 'user__user_pts')
@@ -38,6 +48,15 @@ class CTFchallengeViewSet(viewsets.ModelViewSet):
         
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        # get_queryset 메서드 오버라이드: 특정 CTF에 속한 챌린지들만 조회
+
+'''
+class CTFChallengeListView(APIView):
+    def get(self, request, ctf_id):
+        ctf_challenges = CTF_challenge.objects.filter(ctf_id=ctf_id)
+        serializer = CTFchallengeSerializer(ctf_challenges, many=True)
+        return Response(serializer.data)
+'''
 
 
 
@@ -76,20 +95,4 @@ class SubmitCTFFlagAPI(APIView):
             return Response({'message': '틀렸습니다. 다시 시도하세요.'}, status=400)
 
         
-
-
-#길드에서 ctf관리
-#ctf 안의 문제들은 ctf가 종료되고 난 후에 워게임으로 이전하지 않음
-#ctf 개최
-
-
-
-#ctf삭제 
-#ctf 문제 보기,제출... ->워게임꺼 가져오기.
-
-
-#ctf 생성....
-#실시간 랭킹기능 (?)
-
-#ctf 참가하기 -> 참가하면 이상한 모델 생성~~~
 
