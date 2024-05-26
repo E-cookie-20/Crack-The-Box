9093997ec018af4b98c682e67bf2b78e4b765856
rkdmsslasla <gaeun9477@gmail.com>
2024-05-26 21:08:30 +0900

    jwt토큰 로그인 개선(ustomTokenObtainPairSerializer)

--- backend/config/users/views.py
+++ backend/config/users/views.py
@@ -1,13 +1,12 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from .serializers import UsersSerializer
-# from .serializers import UserLoginSerializer
-from .models import User
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import User
+from .serializers import UsersSerializer
+from .serializers import CustomTokenObtainPairSerializer
 
 class UsersViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -16,14 +15,16 @@ class UsersViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save()
 
-    
-"""
-def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-"""
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+# class UserDetailView(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def get(self, request):
+#         user = request.user
+#         serializer = UsersSerializer(user)
+#         return Response(serializer.data)
 
 
