fb55c89be1c63c3e2e0eac09d989850b4868b68f
박지은 <31093793+lucykorea414@users.noreply.github.com>
2024-06-07 20:18:12 +0900

    Update CTFDetail.js

--- client/src/components/CTFDetail.js
+++ client/src/components/CTFDetail.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CTFProblem from "../pages/CTFProblem";
 import Leaderboard from "./LeaderBoard";
+import axios from 'axios';
 
 const CTFDetail = ({
   id,
