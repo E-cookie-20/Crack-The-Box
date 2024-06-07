20491f2df6a8bf998131b04adffec5fcac77b0a0
rkdmsslasla <gaeun9477@gmail.com>
2024-06-07 19:24:07 +0900

    MyHeader.js 이슈 해결

--- client/src/components/MyHeader.js
+++ client/src/components/MyHeader.js
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext"; // useAuth 훅 import
 import logo from "../assets/crack_the_box_logo.png";
 
 const MyHeader = () => {
-  const [active, setActive] = useState(""); // 현재 활성화된 메뉴를 추적하는 상태
   const navigate = useNavigate();
   const { isLoggedIn, logout } = useAuth(); // useAuth 훅을 사용하여 isLoggedIn 상태와 logout 함수 가져오기
 
-  const handleClick = (menu) => {
-    setActive(menu);
-    navigate(`/${menu}`, { replace: true });
+  const clickLogo = () => {
+    navigate("/", { replace: true });
   };
-
+  const clickGuild = () => {
+    navigate("/guild", { replace: true });
+  };
+  const clickWargame = () => {
+    navigate("/wargame", { replace: true });
+  };
+  const clickLogin = () => {
+    navigate("/login", { replace: true });
+  };
+  const clickSignup = () => {
+    navigate("/signup", { replace: true });
+  };
   const handleLogout = () => {
     logout(); // 로그아웃 함수 호출
     navigate("/", { replace: true }); // 홈페이지로 이동
@@ -27,26 +36,12 @@ const MyHeader = () => {
               alt="logo"
               className="logo"
               src={logo}
-              onClick={() => handleClick("")}
+              onClick={clickLogo}
             ></img>
           </div>
           <div className="nav_1">
-            <div className="nav_1_1">
-              <text
-                className={active === "wargame" ? "active" : ""}
-                onClick={() => handleClick("wargame")}
-              >
-                워게임
-              </text>
-            </div>
-            <div className="nav_1_2">
-              <text
-                className={active === "guild" ? "active" : ""}
-                onClick={() => handleClick("guild")}
-              >
-                길드
-              </text>
-            </div>
+            <span className="header_text" onClick={clickWargame}>워게임</span>
+            <span className="header_text" onClick={clickGuild}>길드</span>
           </div>
         </div>
         <div className="nav_2">
@@ -54,11 +49,8 @@ const MyHeader = () => {
             <button onClick={handleLogout}>로그아웃</button>
           ) : (
             <>
-              <text onClick={() => handleClick("login")}>로그인</text>
-              <button
-                className="signup_button"
-                onClick={() => handleClick("signup")}
-              >
+              <span className="header_text" onClick={clickLogin}>로그인</span>
+              <button className="signup_button" onClick={clickSignup}>
                 회원가입하고 바로 시작하기
               </button>
             </>
