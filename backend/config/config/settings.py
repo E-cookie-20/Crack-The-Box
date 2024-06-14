a72a7e799388c64764ec7cfe82679f9d404b882e
박지은 <31093793+lucykorea414@users.noreply.github.com>
2024-06-15 01:15:53 +0900

    Update settings.py

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -158,7 +158,7 @@ CORS_ALLOWED_ORIGINS = [
 #     "http://127.0.0.1:3000",
 # }
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
